6cc87b4efff6f23fd5cb4984866684b5e38f651e
Max <max@maxx230.home>
2020-02-16 17:32:10 +0100

    Change wallpaper repeatedly

--- randomWallpaper.py
+++ randomWallpaper.py
@@ -1,20 +1,19 @@
 import os
 import glob
 import subprocess
+import time
 from secrets import randbelow
 
 #####################################
 # Wallpaper folder
 path = os.path.expanduser("~") + "/Desktop/Workspace/Wallpaper"
+# Should the commands run repeatedly
+runRepeatedly = True
+# Repeat seconds
+secondsBetweenRuns = 10.0
 #####################################
 
-if __name__ == "__main__":
-    # Go to folder which should contain all wallpaper files
-    os.chdir(path)
-
-    # Get all files from specified directory as array
-    pictureArray = glob.glob("*")
-
+def showRandomPicture(pictureArray):
     # Create random number
     random = randbelow(pictureArray.__len__())
 
@@ -28,4 +27,18 @@ if __name__ == "__main__":
     # Thanks to https://stackoverflow.com/questions/4256107/running-bash-commands-in-python
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+
+
+if __name__ == "__main__":
+     # Go to folder which should contain all wallpaper files
+    os.chdir(path)
+
+    # Get all files from specified directory as array
+    pictureArray = glob.glob("*")
+
+    starttime = time.time()
+    while runRepeatedly:
+        showRandomPicture(pictureArray)
+        print(time.time())
+        time.sleep(secondsBetweenRuns - ((time.time() - starttime) % secondsBetweenRuns))
 
