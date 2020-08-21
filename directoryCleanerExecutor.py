f3a1e315f11cfad0151fe41338d857fa71196da4
Max Jänicke <max.jaenicke@gmx.de>
2020-08-22 00:31:02 +0200

    add requirements with dynamic programming

---
+++ directoryCleanerExecutor.py
@@ -0,0 +1,25 @@
+# Define pattern of file who should be deleted and standard direcory
+
+
+# Ask user if another directory should be used
+
+
+# Input fom user / could be explorer
+
+
+# Validate that input is a directory
+
+
+# Check every 20 seconds
+
+
+# Print every file which could be deleted in console
+
+
+# Ask user for confirmation
+
+
+# (Optional) Create backup folder if not already exists
+
+
+# Delete files with pattern from operating system
---
+++ houseCalculation.py
@@ -0,0 +1,25 @@
+# Define variables for an house calculation tool
+
+
+# Define example house calculation
+
+
+# (Optional) Import house calculation variables
+
+
+# Ask user for input for each variable
+
+
+# Validate variables
+
+
+# (Optional) Save variables for later usage
+
+
+# Calculate values
+
+
+# Create excel file
+
+
+# Write values to excel file
