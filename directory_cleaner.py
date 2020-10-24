a833428383756326efb98259e8705edd63ccf793
Max Jänicke <max.jaenicke@gmx.de>
2020-10-24 19:26:20 +0200

    worked on directory cleaner

--- directory_cleaner.py
+++ directory_cleaner.py
@@ -1,25 +1,32 @@
-# Define pattern of file who should be deleted and standard direcory
-
-
-# Ask user if another directory should be used
-
-
-# Input fom user / could be explorer
-
-
-# Validate that input is a directory
-
-
-# Check every 20 seconds
-
-
-# Print every file which could be deleted in console
-
-
-# Ask user for confirmation
-
-
+# (Optional) Define pattern of file who should be deleted and standard direcory
+# (Optional) Ask user if another directory should be used
+# (Optional) Input fom user / could be explorer
+# (Optional) Validate that input is a directory
+# (Optional) Ask user for confirmation
 # (Optional) Create backup folder if not already exists
+# (Optional) Delete files with pattern from operating system
 
+# https://pypi.org/project/schedule/
+import schedule, time, glob, os, send2trash
+from pathlib import Path
 
-# Delete files with pattern from operating system
+HOME_DIR = str(Path.home())
+DEL_EXTENSIONS = ['.txt', '.log']
+
+def clean_directory():    
+    os.chdir(HOME_DIR)
+    print(f"Check for new files in directory {os.getcwd()}")
+
+    files = os.listdir()
+    for file in files:
+        if os.path.splitext(file)[1] in DEL_EXTENSIONS:
+            send2trash.send2trash(file)
+            print(F'Deleted file {file}')
+
+if __name__ == "__main__":
+    schedule.every(30).seconds.do(clean_directory)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        
