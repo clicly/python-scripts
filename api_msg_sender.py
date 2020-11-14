29d7b5d3f1c40927de9c291afdc329d5274a3c4d
Max Jänicke <max.jaenicke@gmx.de>
2020-11-14 09:20:48 +0100

    minor refactoring

--- api_msg_sender.py
+++ api_msg_sender.py
@@ -17,27 +17,25 @@ class ApiRequest:
     AFFIRMATION = 2
     DADJOKE = 3
 
-def request_dog_picture_api():
-    r = requests.get("https://api.thedogapi.com/v1/images/search")
+def request_api(api_url):
+    r = requests.get(api_url)
     r.raise_for_status()
-    json_object = json.loads(r.content)
+    return json.loads(r.content)
+
+def request_dog_picture_api():
+    json_object = request_api("https://api.thedogapi.com/v1/images/search")
     # more information about the dog available in json object
-    return json_object["url"][0]
+    return json_object[0]["url"]
 
 def request_affirmation_api():
-    r = requests.get("https://www.affirmations.dev/")
-    r.raise_for_status()
-    json_object = json.loads(r.content)
+    json_object = request_api("https://www.affirmations.dev/")
     return json_object["affirmation"]
 
 def request_dadjoke_api():
-    r = requests.get("https://icanhazdadjoke.com/slack")
-    r.raise_for_status()
-    json_object = json.loads(r.content)
+    json_object = request_api("https://icanhazdadjoke.com/slack")
     return json_object["attachments"][0]["text"]
 
-# only used for bot setup to get the chat id
-# will not be used in the future
+# will not be used in the future, only used for bot setup to get the chat id 
 def get_chat_id(token):
     url = f'https://api.telegram.org/bot{token}/getUpdates'
     print(requests.post(url).json())
@@ -63,7 +61,7 @@ def send_msg_via_email(user_mail_address, user_password, recipients_emails, mail
     finally:
         connection.quit()
 
-def request_api(api_request):
+def choose_api(api_request):
     if (api_request == ApiRequest.DOGS):
         return request_dog_picture_api()
     elif (api_request == ApiRequest.AFFIRMATION):
@@ -73,7 +71,7 @@ def request_api(api_request):
     raise NotImplementedError()
 
 if __name__ == "__main__":  
-    msg = request_api(ApiRequest.AFFIRMATION)
+    msg = choose_api(ApiRequest.DOGS)
 
     send_msg_via_telegram(CHAT_ID, TOKEN, msg)
     # send_msg_via_email(USER_MAIL_ADDRESS, USER_PASSWORD, RECIPIENTS_EMAILS, msg)
